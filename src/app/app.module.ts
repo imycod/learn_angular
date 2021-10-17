d824051b17193da1228edbbf3aa25575346a3aaf
imycod <317009160@qq.com>
2021-10-17 11:09:11 +0800

    add HttpClientInMemoryWebApiModule at imports HttpClientModule after and InMemoryDataService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api'
+import {InMemoryDataService} from './in-memory-data.service'
 
 @NgModule({
     declarations: [
@@ -10,6 +12,7 @@ import {HttpClientModule} from "@angular/common/http";
     ],
     imports: [
         HttpClientModule,
+        HttpClientInMemoryWebApiModule,
         BrowserModule,
         AppRoutingModule,
     ],
