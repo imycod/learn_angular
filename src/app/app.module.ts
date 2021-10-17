7606baf3223a1fbe8144ddf19edb4219da0bafec
imycod <317009160@qq.com>
2021-10-17 10:47:44 +0800

    add HttpClientModule at imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
     declarations: [
         AppComponent,
     ],
     imports: [
-
+        HttpClientModule,
         BrowserModule,
         AppRoutingModule,
     ],
@@ -17,3 +18,6 @@ import {AppComponent} from './app.component';
 })
 export class AppModule {
 }
+
+
+// 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
